c9857477955edb1eaa731531f6bfa170307529c3
Daniel Gustafsson <daniel@yesql.se>
2016-10-04 14:41:39 +0200

    WIP: Add support for macOS Secure Transport SSL library
    This is a work in progress trying to add full support for Secure
    Transport to the front- and backend. Currently only the frontend
    is worked on in an attempt to start small (or rather, somewhere).

--- src/test/ssl/t/001_ssltests.pl
+++ src/test/ssl/t/001_ssltests.pl
@@ -59,7 +59,7 @@ chmod 0600, "ssl/client_tmp.key";
 #### Part 0. Set up the server.
 
 note "setting up data directory";
-my $node = get_new_node('master');
+my $node = get_new_node('master', $ENV{SSLTEST_SERVER_BIN});
 $node->init;
 
 # PGHOST is enforced here to set up the node, subsequent connections
