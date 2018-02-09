93fb5e302983da4d1e99ed566fd2a899b6cf5900
Magnus Hagander <magnus@hagander.net>
2018-02-09 15:57:41 +0100

    Cleanup

--- src/backend/postmaster/pgstat.c
+++ src/backend/postmaster/pgstat.c
@@ -3512,9 +3512,6 @@ pgstat_get_wait_activity(WaitEventActivity w)
 		case WAIT_EVENT_WAL_WRITER_MAIN:
 			event_name = "WalWriterMain";
 			break;
-		case WAIT_EVENT_CHECKSUMHELPER_LAUNCHER_MAIN:
-			event_name = "ChecksumHelper";
-			break;
 			/* no default case, so that compiler will warn */
 	}
 
